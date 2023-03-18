1288b2162e3d238ce62332deb4564bd31359a3e0
anshchawla521 <anshchawla521@gmail.com>
2023-03-19 02:56:38 +0530

    some visual changes

--- Hovercraft.ino
+++ Hovercraft.ino
@@ -27,11 +27,12 @@
 #define ROLL 0
 #define ELEVATOR 1
 #define THROTTLE 2
+#define RUDDER 2
 #define ARM 4
 
-#define INFLATE 1
-#define THRUSTER 2
-#define RUDDER 3
+#define INFLATE_PWM_CH 1
+#define THRUSTERC_PWM_CH 2
+#define RUDDERC_PWM_CH 3
 
 #define INFLATE_PIN 23
 #define THRUSTER_PIN 25
@@ -39,9 +40,9 @@
 
 #define PWM_FREQUENCY 50
 #define PWM_RESOLUTION 12
-#define PWM_MIN 206
-#define PWM_MAX 406
-#define PWM_MID 306
+#define PWM_MIN 206 // corresponds to 0 degress
+#define PWM_MAX 406 // corresponds to 180 degrees
+#define PWM_MID 306 // corresponds to 90 degrees
 
 #define SBUS_RX 16
 #define SBUS_TX 17
@@ -56,29 +57,32 @@ int throttle_val = PWM_MIN;
 int thrust_val = PWM_MIN;
 int rudder_val = PWM_MID;
 
-int max_ch3 = 1811;
-int max_ch2 = 1811;
-int max_ch1 = 1811;
-int max_ch0 = 1748;
 
-int min_ch3 = 172;
-int min_ch2 = 172;
-int min_ch1 = 172;
-int min_ch0 = 241;
+
+/////////////////////////////////////////////////User Specified Data /////////////////////////////////////////////
+int max_ch_rudder = 1811;
+int max_ch_throttle = 1811;
+int max_ch_elevator = 1811;
+int max_ch_aileron = 1748;
+
+int min_ch_rudder = 172;
+int min_ch_throttle = 172;
+int min_ch_elevator = 172;
+int min_ch_aileron = 241;
 
 
 void setup() {
   //Setup Infaltion motor on pin 23
-  ledcSetup(INFLATE,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(INFLATE_PIN,INFLATE);
+  ledcSetup(INFLATE_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
+  ledcAttachPin(INFLATE_PIN,INFLATE_PWM_CH);
 
   //Setup Thruster motor on pin 25
-  ledcSetup(THRUSTER,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(THRUSTER_PIN,THRUSTER);
+  ledcSetup(THRUSTERC_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
+  ledcAttachPin(THRUSTER_PIN,THRUSTERC_PWM_CH);
 
   //Setup Rudder servo on pin 21
-  ledcSetup(RUDDER,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(RUDDER_PIN,RUDDER);
+  ledcSetup(RUDDERC_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
+  ledcAttachPin(RUDDER_PIN,RUDDERC_PWM_CH);
 
   //Debugging
   Serial.begin(115200);
@@ -101,15 +105,16 @@ void loop () {
     }
     else {
       if(!data.lost_frame){
-        throttle_val = map(constrain(data.ch[THROTTLE], min_ch2 , max_ch2), min_ch2, max_ch2, PWM_MIN, PWM_MAX);
-        thrust_val = map(constrain(data.ch[ELEVATOR], min_ch0, max_ch0), min_ch0, max_ch0, PWM_MIN, PWM_MAX);
-        rudder_val = map(constrain(data.ch[ROLL], min_ch0, max_ch0), min_ch0, max_ch0, PWM_MIN, PWM_MAX);
+        throttle_val = map(constrain(data.ch[THROTTLE], min_ch_throttle , max_ch_throttle), min_ch_throttle, max_ch_throttle, PWM_MIN, PWM_MAX);
+        thrust_val = map(constrain(data.ch[ELEVATOR], min_ch_aileron, max_ch_aileron), min_ch_aileron, max_ch_aileron, PWM_MIN, PWM_MAX);
+        rudder_val = map(constrain(data.ch[ROLL], min_ch_aileron, max_ch_aileron), min_ch_aileron, max_ch_aileron, PWM_MIN, PWM_MAX);
       }
     }
 
-    ledcWrite(INFLATE,throttle_val);
-    ledcWrite(THRUSTER,thrust_val);
-    ledcWrite(RUDDER, rudder_val);
+    // write values to motors
+    ledcWrite(INFLATE_PWM_CH,throttle_val);
+    ledcWrite(THRUSTERC_PWM_CH,thrust_val);
+    ledcWrite(RUDDERC_PWM_CH, rudder_val);
 
 
 //debugging
@@ -126,8 +131,8 @@ void loop () {
 
 void calibrate(){
   Serial.println("Rotate sticks in all directions");
-  int max_ch3 = 1000 , max_ch2 = 1000 , max_ch1 = 1000 , max_ch0 = 1000;
-  int min_ch3 = 1000 , min_ch2 = 1000 , min_ch1 = 1000 , min_ch0 = 1000;
+  int max_ch_rudder = 1000 , max_ch_throttle = 1000 , max_ch_elevator = 1000 , max_ch_aileron = 1000;
+  int min_ch_rudder = 1000 , min_ch_throttle = 1000 , min_ch_elevator = 1000 , min_ch_aileron = 1000;
 
   unsigned t = millis();
   while (true and millis() - t < 10000)
@@ -135,26 +140,26 @@ void calibrate(){
     if (sbus_rx.Read()) {
       data = sbus_rx.data();
       Serial.println("Receiving data");
-      max_ch0 = data.ch[0] > max_ch0 ? data.ch[0] : max_ch0;
-      max_ch1 = data.ch[1] > max_ch1 ? data.ch[1] : max_ch1;
-      max_ch2 = data.ch[2] > max_ch2 ? data.ch[2] : max_ch2;
-      max_ch3 = data.ch[3] > max_ch3 ? data.ch[3] : max_ch3;
+      max_ch_aileron = data.ch[ROLL] > max_ch_aileron ? data.ch[ROLL] : max_ch_aileron;
+      max_ch_elevator = data.ch[ELEVATOR] > max_ch_elevator ? data.ch[ELEVATOR] : max_ch_elevator;
+      max_ch_throttle = data.ch[THROTTLE] > max_ch_throttle ? data.ch[THROTTLE] : max_ch_throttle;
+      max_ch_rudder = data.ch[RUDDER] > max_ch_rudder ? data.ch[RUDDER] : max_ch_rudder;
 
-      min_ch0 = data.ch[0] < min_ch0 ? data.ch[0] : min_ch0;
-      min_ch1 = data.ch[1] < min_ch1 ? data.ch[1] : min_ch1;
-      min_ch2 = data.ch[2] < min_ch2 ? data.ch[2] : min_ch2;
-      min_ch3 = data.ch[3] < min_ch3 ? data.ch[3] : min_ch3;
+      min_ch_aileron = data.ch[ROLL] < min_ch_aileron ? data.ch[ROLL] : min_ch_aileron;
+      min_ch_elevator = data.ch[ELEVATOR] < min_ch_elevator ? data.ch[ELEVATOR] : min_ch_elevator;
+      min_ch_throttle = data.ch[THROTTLE] < min_ch_throttle ? data.ch[THROTTLE] : min_ch_throttle;
+      min_ch_rudder = data.ch[RUDDER] < min_ch_rudder ? data.ch[RUDDER] : min_ch_rudder;
     }
 
   }
   Serial.println("Calibration done paste this in code");
-  Serial.print("int max_ch3 = "); Serial.print(max_ch3); Serial.println(";");
-  Serial.print("int max_ch2 = "); Serial.print(max_ch2); Serial.println(";");
-  Serial.print("int max_ch1 = "); Serial.print(max_ch1); Serial.println(";");
-  Serial.print("int max_ch0 = "); Serial.print(max_ch0); Serial.println(";");
+  Serial.print("int max_ch_rudder = "); Serial.print(max_ch_rudder); Serial.println(";");
+  Serial.print("int max_ch_throttle = "); Serial.print(max_ch_throttle); Serial.println(";");
+  Serial.print("int max_ch_elevator = "); Serial.print(max_ch_elevator); Serial.println(";");
+  Serial.print("int max_ch_aileron = "); Serial.print(max_ch_aileron); Serial.println(";");
 
-  Serial.print("int min_ch3 = "); Serial.print(min_ch3); Serial.println(";");
-  Serial.print("int min_ch2 = "); Serial.print(min_ch2); Serial.println(";");
-  Serial.print("int min_ch1 = "); Serial.print(min_ch1); Serial.println(";");
-  Serial.print("int min_ch0 = "); Serial.print(min_ch0); Serial.println(";");
+  Serial.print("int min_ch_rudder = "); Serial.print(min_ch_rudder); Serial.println(";");
+  Serial.print("int min_ch_throttle = "); Serial.print(min_ch_throttle); Serial.println(";");
+  Serial.print("int min_ch_elevator = "); Serial.print(min_ch_elevator); Serial.println(";");
+  Serial.print("int min_ch_aileron = "); Serial.print(min_ch_aileron); Serial.println(";");
 }
