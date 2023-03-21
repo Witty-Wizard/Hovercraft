e7e7f3f31f9991d9333062da4fd2a8765dfab0c4
anshchawla521 <anshchawla521@gmail.com>
2023-03-21 22:42:56 +0530

    implmented running average bt without time cosnideration

--- Hovercraft.ino
+++ Hovercraft.ino
@@ -34,15 +34,30 @@
 #define THRUSTERC_PWM_CH 2
 #define RUDDERC_PWM_CH 3
 
-#define INFLATE_PIN 23
-#define THRUSTER_PIN 25
-#define RUDDER_PIN 21
+#define INFLATE_PIN 19
+#define THRUSTER_PIN 18
+#define RUDDER_PIN 4
 
+
+
+/////////////////////////////////////PWM Config
 #define PWM_FREQUENCY 50
 #define PWM_RESOLUTION 12
 #define PWM_MIN 206 // corresponds to 0 degress
 #define PWM_MAX 406 // corresponds to 180 degrees
 #define PWM_MID 306 // corresponds to 90 degrees
+
+#define INFLATE_PWM_MAX 370
+#define THRUSTER_PWM_MAX 370
+
+///////////////////////////////////LOW pass filter config / integrator
+
+#define total_number_of_samples 200
+int thruster_average[total_number_of_samples] = {0};
+int inflate_average[total_number_of_samples] = {0};
+
+
+// Sbus config
 
 #define SBUS_RX 16
 #define SBUS_TX 17
@@ -53,8 +68,8 @@ bfs::SbusRx sbus_rx(&Serial2, SBUS_RX, SBUS_TX, true);
 /* SBUS data */
 bfs::SbusData data;
 
-int throttle_val = PWM_MIN;
-int thrust_val = PWM_MIN;
+float throttle_val = PWM_MIN;
+float thrust_val = PWM_MIN;
 int rudder_val = PWM_MID;
 
 
@@ -73,16 +88,16 @@ int min_ch_aileron = 241;
 
 void setup() {
   //Setup Infaltion motor on pin 23
-  ledcSetup(INFLATE_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(INFLATE_PIN,INFLATE_PWM_CH);
+  ledcSetup(INFLATE_PWM_CH, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcAttachPin(INFLATE_PIN, INFLATE_PWM_CH);
 
   //Setup Thruster motor on pin 25
-  ledcSetup(THRUSTERC_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(THRUSTER_PIN,THRUSTERC_PWM_CH);
+  ledcSetup(THRUSTERC_PWM_CH, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcAttachPin(THRUSTER_PIN, THRUSTERC_PWM_CH);
 
   //Setup Rudder servo on pin 21
-  ledcSetup(RUDDERC_PWM_CH,PWM_FREQUENCY,PWM_RESOLUTION);
-  ledcAttachPin(RUDDER_PIN,RUDDERC_PWM_CH);
+  ledcSetup(RUDDERC_PWM_CH, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcAttachPin(RUDDER_PIN, RUDDERC_PWM_CH);
 
   //Debugging
   Serial.begin(115200);
@@ -92,32 +107,55 @@ void setup() {
   sbus_rx.Begin();
 }
 
+// add failsafe time of 2 sec
+
 void loop () {
   if (sbus_rx.Read()) {
     data = sbus_rx.data();
 
+    for (int i = total_number_of_samples - 1; i > 0; i--) {
+      // shifting of values
+      inflate_average[i] = inflate_average[i - 1];
+      thruster_average[i] = thruster_average[i - 1];
+
+
+    }
     // ARM on channel 5
-    if ((data.ch[ARM] < 1000) || data.failsafe ){
+    if ((data.ch[ARM] < 1000) || data.failsafe ) {
       throttle_val = PWM_MIN;
       thrust_val = PWM_MIN;
       rudder_val = PWM_MID;
-      
+
     }
     else {
-      if(!data.lost_frame){
-        throttle_val = map(constrain(data.ch[THROTTLE], min_ch_throttle , max_ch_throttle), min_ch_throttle, max_ch_throttle, PWM_MIN, PWM_MAX);
-        thrust_val = map(constrain(data.ch[ELEVATOR], min_ch_aileron, max_ch_aileron), min_ch_aileron, max_ch_aileron, PWM_MIN, PWM_MAX);
+      if (!data.lost_frame) {
+        // added low pass filter so ESC doesnt blow up because of high currents
+        inflate_average[0] = map(constrain(data.ch[THROTTLE], min_ch_throttle , max_ch_throttle), min_ch_throttle, max_ch_throttle, PWM_MIN, INFLATE_PWM_MAX);
+        thruster_average[0] = map(constrain(data.ch[ELEVATOR], min_ch_aileron, max_ch_aileron), min_ch_aileron, max_ch_aileron, PWM_MIN, THRUSTER_PWM_MAX);
+
         rudder_val = map(constrain(data.ch[ROLL], min_ch_aileron, max_ch_aileron), min_ch_aileron, max_ch_aileron, PWM_MIN, PWM_MAX);
+
+
+
       }
     }
 
+    // compute average
+
+    for (int i = 0; i < total_number_of_samples ; i++) {
+      // shifting of values
+      throttle_val += inflate_average[i]/total_number_of_samples;
+      thrust_val += thruster_average[i]/total_number_of_samples;
+
+
+    }
     // write values to motors
-    ledcWrite(INFLATE_PWM_CH,throttle_val);
-    ledcWrite(THRUSTERC_PWM_CH,thrust_val);
+    ledcWrite(INFLATE_PWM_CH, throttle_val);
+    ledcWrite(THRUSTERC_PWM_CH, thrust_val);
     ledcWrite(RUDDERC_PWM_CH, rudder_val);
 
 
-//debugging
+    //debugging
     for (int8_t i = 0; i < 3; i++) {
       Serial.print(data.ch[i]);
       Serial.print("\t");
@@ -129,7 +167,7 @@ void loop () {
   }
 }
 
-void calibrate(){
+void calibrate() {
   Serial.println("Rotate sticks in all directions");
   int max_ch_rudder = 1000 , max_ch_throttle = 1000 , max_ch_elevator = 1000 , max_ch_aileron = 1000;
   int min_ch_rudder = 1000 , min_ch_throttle = 1000 , min_ch_elevator = 1000 , min_ch_aileron = 1000;
