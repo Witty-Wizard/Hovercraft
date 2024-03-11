8670b2dd65126c10e80aa2731a7c5cfbf7419796
Witty-Wizard <shashankmarch27@gmail.com>
2024-03-11 22:02:07 +0530

    added read me file

--- src/hovercraft.cpp
+++ src/hovercraft.cpp
@@ -1,26 +1,11 @@
-/*
-  Robotics Pec
-  robotics@pec.edu.in
-
-                     GNU GENERAL PUBLIC LICENSE
-                        Version 3, 29 June 2007
-
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Everyone is permitted to copy and distribute verbatim copies
-  of this license document, but changing it is not allowed.
-
-  The GNU General Public License is a free, copyleft license for
-  software and other kinds of works.
-
-  THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
-  APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
-  HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
-  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
-  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
-  IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
-  ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
-*/
+/**
+ * @file main.cpp
+ * @brief Main application file.
+ * @author Robotics Society
+ * @date March 11, 2024
+ *
+ * @license GNU General Public License Version 3
+ */
 
 #include <Arduino.h>
 #include <crsf/crsf.h>
